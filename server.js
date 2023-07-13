cb6cee4823dc4ba72d3d30a6bbde1d7797abfd08
Jayden <lygiahuy001122@gmail.com>
2023-07-13 17:47:44 -0600

    Express project

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
-const http = require('http');
+const express = require('express')
+const app = express()
+const port = 3000
 
-const hostname = '127.0.0.1';
-const port = 3000;
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+//config template engine
+app.set('views', './src/views')
+app.set('view engine', 'ejs')
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n Create my first server');
-});
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.get('/abc', (req, res) => {
+  res.send('Hello Jayden!')
+})
+app.get('/Jayden', (req, res) => {
+  //res.send('<h1>Hello Jayden!</h1>')
+  res.render('sample.ejs')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
